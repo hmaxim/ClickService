e8b34d73ca73c35b4991459ba8028e4c6b9c2df8
hmaxim <maxim.haprinin@yandex.ru>
2017-03-19 14:54:29 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$('document').ready(function(){
+	    $('#modal').modal();
+	});
+
+
+function initialize() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		var latlng = new google.maps.LatLng(latitude, longitude);
+		var settings = {
+			zoom: 15,
+			center: latlng,
+			mapTypeControl: true,
+			mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+			navigationControl: true,
+			navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("map_canvas"), 
+			settings);
+
+		var emarker = new google.maps.Marker({
+			position:latlng,
+			map: map,
+			title: "You are here"
+		});
+	});
+};
