bdb53fb40efaa7275760beffd9f2d28a787484c5
hmaxim <maxim.haprinin@yandex.ru>
2017-05-03 21:14:01 +0300

    Update link response & part of client google maps
    add google maps with api key & markers on maps

--- script/private_client_script.js
+++ script/private_client_script.js
@@ -1,43 +1,15 @@
 $('document').ready(function(){
 	getUserInfo();
-	initialize();
 });
-
-//its works
-function initialize() {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		var latitude = position.coords.latitude;
-		var longitude = position.coords.longitude;
-		var latlng = new google.maps.LatLng(latitude, longitude);
-		var settings = {
-			zoom: 15,
-			center: latlng,
-			mapTypeControl: true,
-			mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-			navigationControl: true,
-			navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		var map = new google.maps.Map(document.getElementById("map_canvas"), 
-			settings);
-
-		var emarker = new google.maps.Marker({
-			position:latlng,
-			map: map,
-			title: "You are here"
-		});
-	});
-}
 
 function getUserInfo() {
 	var token = localStorage.getItem("userToken");
-	var clientID = localStorage.getItem("clientID");
 	$.ajax({
 		beforeSend: function(req) {
-			req.setRequestHeader("Authorization", "Bearer " + token  );
+			req.setRequestHeader("Authorization",token);
 		},
 		method: 'GET',
-		url: 'https://hair-salon-personal.herokuapp.com/service/clients/'+clientID,
+		url: 'https://hair-salon-personal.herokuapp.com/client/info/',
 		contentType: "application/json; charset=utf-8",
 		success: function(data) {
 			console.log(data.clientName);
@@ -48,3 +20,65 @@ function getUserInfo() {
 		}
 	});
 }
+
+var inputName = $('#client_name');
+var inputLName = $('#client_lName');
+var inputPhone = $('#client_phone');
+var inputPassword = $('#client_pass');
+$('#edit_btn').click(function(e) {
+  e.preventDefault();
+  if (inputName.prop('disabled')) {
+    inputName.prop('disabled', false);
+    inputLName.prop('disabled', false);
+    inputPhone.prop('disabled', false);
+    inputPassword.show();
+    inputPassword.prop('disabled',false);
+    $('#submit_edit_btn').show();
+    $('#edit_btn').hide();
+  } else {
+    inputName.prop('disabled', true);
+    inputLName.prop('disabled', true);
+    inputPhone.prop('disabled', true);
+  }
+});
+
+$('#submit_edit_btn').click(function(e){
+    e.preventDefault();
+    inputName.prop('disabled', true);
+    inputLName.prop('disabled', true);
+    inputPhone.prop('disabled', true);
+    inputPassword.prop('disabled',true);
+    inputPassword.hide();
+    $('#submit_edit_btn').hide();
+    $('#edit_btn').show();
+
+	var token = localStorage.getItem("userToken");
+	var name = $('#client_name').val();
+    var lastName = $('#client_lName').val();
+    var phone = $('#client_phone').val();
+    var email = $('#client_email').val();
+    var password = $('#client_pass').val();
+
+    var updateClient = {
+      clientName: name,
+      clientLastName: lastName,
+      clientPhoneNumber: phone,
+      clientEmail: email
+    }
+
+   var str = JSON.stringify(updateClient);
+   var token = localStorage.getItem("userToken");
+	  $.ajax({
+	    beforeSend: function(req) {
+	      req.setRequestHeader("Authorization", token);
+	    },
+	    method: 'PUT',
+	    url: 'https://hair-salon-personal.herokuapp.com/client/update',
+	    contentType: "application/json; charset=utf-8",
+	    data: str,
+	    success: function(res) {
+	      console.log(res);
+	    }
+  });
+
+});
